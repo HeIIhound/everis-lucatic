af4e71eee5fb70c3b79351708d64ffa85e7e0ccb
HeIIhound <orlandopereira1233@hotmail.com>
2018-06-20 17:57:57 +0200

    20/06/2018 v2
    agregada la capa datos
    clase datos creada
    interfaz datos creada
    
    agregada la capa servicios
    clase servicios creada
    interfaz servicios creada
    
    agregada y comentada la configuracion de hibernate en
    ApplicationContextConfig

--- banco-app/src/main/java/spring/configuration/ApplicationContextConfig.java
+++ banco-app/src/main/java/spring/configuration/ApplicationContextConfig.java
@@ -1,30 +1,83 @@
 package spring.configuration;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import spring.model.Cuenta;
+import spring.model.Movimiento;
+import spring.model.Usuario;
+import spring.model.Usuariologin;
+
 @EnableWebMvc
 @Configuration
-@ComponentScan(basePackages="spring")
-public class ApplicationContextConfig extends WebMvcConfigurerAdapter{
-	
-    @Bean
-    public InternalResourceViewResolver getViewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/view/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
-    }
-    
-    @Override
+@ComponentScan(basePackages = "spring")
+@EnableTransactionManagement
+public class ApplicationContextConfig extends WebMvcConfigurerAdapter {
+
+	@Bean(name = "viewResolver")
+	public InternalResourceViewResolver getViewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
 
-}
+//	@Bean(name = "dataSource")
+//	public DataSource getDataSource() {
+//		BasicDataSource dataSource = new BasicDataSource();
+//		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//		dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/banco");
+//		dataSource.setUsername("root");
+//		dataSource.setPassword("");
+//
+//		return dataSource;
+//	}
+//
+//	private Properties getHibernateProperties() {
+//		Properties properties = new Properties();
+//		properties.put("hibernate.show_sql", "true");
+//		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+//		return properties;
+//	}
+//
+//	@Autowired
+//	@Bean(name = "sessionFactory")
+//	public SessionFactory getSessionFactory(DataSource dataSource) {
+//		LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+//		sessionBuilder.addProperties(getHibernateProperties());
+//		sessionBuilder.addAnnotatedClasses(Cuenta.class);
+//		sessionBuilder.addAnnotatedClasses(Movimiento.class);
+//		sessionBuilder.addAnnotatedClasses(Usuario.class);
+//		sessionBuilder.addAnnotatedClasses(Usuariologin.class);
+//		return sessionBuilder.buildSessionFactory();
+//	}
+//
+//	@Autowired
+//	@Bean(name = "transactionManager")
+//	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+//		HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
+//
+//		return transactionManager;
+//	}
 
+}
