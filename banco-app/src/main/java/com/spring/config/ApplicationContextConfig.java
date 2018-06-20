660a5048de4980df7513079556ecb2ba830f3c5f
Ddonamaria <daviddonamaria@gmail.com>
2018-06-20 09:50:41 +0200

    20/06/2018

--- banco-app/src/main/java/com/spring/config/ApplicationContextConfig.java
+++ banco-app/src/main/java/com/spring/config/ApplicationContextConfig.java
@@ -39,9 +39,7 @@ public class ApplicationContextConfig extends WebMvcConfigurerAdapter{
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-		
-	    
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");	    
 	}
 	
 /*
